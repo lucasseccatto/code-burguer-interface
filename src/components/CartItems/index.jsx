d2e095f1eaec37436f29134b6947fd3e68dfb5ad
lucasseccatto <lucasseccatto1@gmail.com>
2023-12-20 15:03:34 -0300

    criado component CartItems

---
+++ src/components/CartItems/index.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+
+import { useCart } from '../../hooks/CartContext'
+import formatCurrency from '../../utils/formatCurrency'
+import { Container, Header, Body, EmptyCart } from './styles'
+
+export function CartItems() {
+  const { cartProducts } = useCart()
+
+  console.log(cartProducts)
+  return (
+    <Container>
+      <Header>
+        <p></p>
+        <p>Itens</p>
+        <p>Preço</p>
+        <p style={{ paddingRight: 30 }}>Quantidade</p>
+        <p>Total</p>
+      </Header>
+
+      {cartProducts && cartProducts.length > 0 ? (
+        cartProducts.map(product => (
+          <Body key={product.id}>
+            <img src={product.url} />
+            <p>{product.name}</p>
+            <p>{formatCurrency(product.price)}</p>
+            <p>{product.quantity}</p>
+            <p>{formatCurrency(product.quantity * product.price)}</p>
+          </Body>
+        ))
+      ) : (
+        <EmptyCart>Carrinho vazio</EmptyCart>
+      )}
+    </Container>
+  )
+}
