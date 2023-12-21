a4739d7d25594d9527c4585f630234e1facdc289
lucasseccatto <lucasseccatto1@gmail.com>
2023-12-21 10:39:32 -0300

    criado component header

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,52 @@
+import React from 'react'
+import { useHistory } from 'react-router-dom'
+
+import Cart from '../../assets/cart.svg'
+import Person from '../../assets/person.svg'
+import {
+  Container,
+  ContainerLeft,
+  PageLink,
+  PageLinkExit,
+  ContainerRight,
+  ContainerText,
+  Line
+} from './styles'
+
+export function Header() {
+  const {
+    push,
+    location: { pathname }
+  } = useHistory()
+
+  return (
+    <Container>
+      <ContainerLeft>
+        <PageLink onClick={() => push('/')} isActive={pathname === '/'}>
+          Home
+        </PageLink>
+        <PageLink
+          onClick={() => push('/produtos')}
+          isActive={pathname.includes('produtos')}
+        >
+          Ver produtos
+        </PageLink>
+      </ContainerLeft>
+
+      <ContainerRight>
+        <PageLink onClick={() => push('/carrinho')}>
+          <img src={Cart} alt="carrinho" />
+        </PageLink>
+        <Line></Line>
+        <PageLink>
+          <img src={Person} alt="logo-pessoa" />
+        </PageLink>
+
+        <ContainerText>
+          <p>Olá, Lucas</p>
+          <PageLinkExit>Sair</PageLinkExit>
+        </ContainerText>
+      </ContainerRight>
+    </Container>
+  )
+}
