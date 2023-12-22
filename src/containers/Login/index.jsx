adb6d30521a12719e2eff104b6f8770bd76dcd28
lucasseccatto <lucasseccatto1@gmail.com>
2023-12-22 11:45:04 -0300

    adicionar validação de administrador para usuarios

--- src/containers/Login/index.jsx
+++ src/containers/Login/index.jsx
@@ -58,7 +58,11 @@ export function Login() {
       putUserData(data)
 
       setTimeout(() => {
-        history.push('/')
+        if (data.admin) {
+          history.push('/pedidos')
+        } else {
+          history.push('/')
+        }
       }, 1000)
     } catch (error) {
       console.error('Erro ao fazer login:', error)
