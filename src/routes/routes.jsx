6d86f8ffb71d5399a16c12f6d0a3ad9c60be2633
lucasseccatto <lucasseccatto1@gmail.com>
2023-12-27 18:25:00 -0300

    refatora rotas: substitui caminhos por constantes

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Switch, Route, BrowserRouter as Router } from 'react-router-dom'
 
+import paths from '../constants/paths'
 import { Admin, Cart, Home, Login, Products, Register } from '../containers'
 import PrivateRoute from './private-route'
 
@@ -8,13 +9,15 @@ function Routes() {
   return (
     <Router>
       <Switch>
-        <Route component={Login} path="/login" />
-        <Route component={Register} path="/cadastro" />
-        <PrivateRoute exact component={Home} path="/" />
-        <PrivateRoute component={Products} path="/produtos" />
-        <PrivateRoute component={Cart} path="/carrinho" />
+        <Route component={Login} path={paths.Login} />
+        <Route component={Register} path={paths.Register} />
+        <PrivateRoute exact component={Home} path={paths.Home} />
+        <PrivateRoute component={Products} path={paths.Products} />
+        <PrivateRoute component={Cart} path={paths.Cart} />
 
-        <PrivateRoute component={Admin} path="/pedidos" isAdmin />
+        <PrivateRoute component={Admin} path={paths.Order} isAdmin />
+        <PrivateRoute component={Admin} path={paths.ListProducts} isAdmin />
+        <PrivateRoute component={Admin} path={paths.NewProduct} isAdmin />
       </Switch>
     </Router>
   )
