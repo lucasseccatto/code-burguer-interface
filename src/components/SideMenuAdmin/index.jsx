7f8ee3d93f0bf2c2a383fe79c8b88676e9e39178
lucasseccatto <lucasseccatto1@gmail.com>
2023-12-27 18:28:15 -0300

    refatoracao: adiciona logica de destaque ao item ativo

--- src/components/SideMenuAdmin/index.jsx
+++ src/components/SideMenuAdmin/index.jsx
@@ -1,4 +1,5 @@
 import LogoutIcon from '@mui/icons-material/Logout'
+import PropTypes from 'prop-types'
 import React from 'react'
 import { Link } from 'react-router-dom'
 
@@ -6,14 +7,14 @@ import { useUser } from '../../hooks/UserContext'
 import listLinks from './menu-list'
 import { Container, ItemContainer, ListLink } from './styles'
 
-export function SideMenuAdmin() {
+export function SideMenuAdmin({ path }) {
   const { logout } = useUser()
 
   return (
     <Container>
       <hr></hr>
       {listLinks.map(item => (
-        <ItemContainer key={item.id} isActive={true}>
+        <ItemContainer key={item.id} isActive={path === item.link}>
           <ListLink to={item.link}>
             <img src={item.icon} alt="icone" />
             {item.label}
@@ -33,4 +34,8 @@ export function SideMenuAdmin() {
       </ItemContainer>
     </Container>
   )
+}
+
+SideMenuAdmin.propTypes = {
+  path: PropTypes.string
 }
--- src/components/SideMenuAdmin/menu-list.js
+++ src/components/SideMenuAdmin/menu-list.js
@@ -1,11 +1,26 @@
 import BagIcon from '../../assets/bag-icon.svg'
+import AddIcon from '../../assets/box-icon.svg'
+import ShopIcon from '../../assets/shop-icon.svg'
+import paths from '../../constants/paths'
 
 const listLinks = [
   {
     id: 1,
     label: 'Pedidos',
-    link: '/pedidos',
+    link: paths.Order,
     icon: BagIcon
+  },
+  {
+    id: 2,
+    label: 'Listar Produtos',
+    link: paths.ListProducts,
+    icon: ShopIcon
+  },
+  {
+    id: 3,
+    label: 'Novo Produto',
+    link: paths.NewProduct,
+    icon: AddIcon
   }
 ]
 
