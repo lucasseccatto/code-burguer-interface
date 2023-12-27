802b937a57c6e4db8daf60861a9db2582e0621dc
lucasseccatto <lucasseccatto1@gmail.com>
2023-12-27 18:30:04 -0300

    conclui criacao de utilitario para gerenciar caminhos

---
+++ src/constants/paths.js
@@ -0,0 +1,13 @@
+const paths = {
+  Login: '/login',
+  Register: '/cadastro',
+  Home: '/',
+  Products: '/produtos',
+  Cart: '/carrinho',
+  Order: '/pedidos',
+  ListProducts: '/listar-produtos',
+  EditProduct: '/editar-produto',
+  NewProduct: '/novo-produto'
+}
+
+export default paths
