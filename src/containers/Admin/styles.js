338449e7413c088c8db3964b6e54eb3736c1b478
lucasseccatto <lucasseccatto1@gmail.com>
2023-12-27 18:26:27 -0300

    refatoracao de admin: estrutura mais dinamica para itens do container

--- src/containers/Admin/styles.js
+++ src/containers/Admin/styles.js
@@ -5,5 +5,10 @@ export const Container = styled.div`
   min-height: 100vh;
   width: 100vw;
   background: #efefef;
-  justify-content: space-between;
+  justify-content: flex-start;
+`
+
+export const ContainerItems = styled.div`
+  padding: 20px;
+  width: 100%;
 `
