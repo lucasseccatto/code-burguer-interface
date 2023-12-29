6b180dfa757cdc125d2a92b1eaf0a038aa3cadc9
lucasseccatto <lucasseccatto1@gmail.com>
2023-12-29 14:50:23 -0300

    refatoracao do componente de register com inclusao do componente errorMessage

--- src/containers/Register/styles.js
+++ src/containers/Register/styles.js
@@ -73,12 +73,3 @@ export const SignInLink = styled.p`
     text-decoration: underline;
   }
 `
-
-export const ErrorMessage = styled.p`
-  font-style: normal;
-  font-weight: normal;
-  font-size: 14px;
-  line-height: 16px;
-  margin-top: 2px;
-  color: #cc1717;
-`
