aa1bafd1f1931847bbc75d0f2847988f07c2c7b8
lucasseccatto <lucasseccatto1@gmail.com>
2023-12-29 14:52:14 -0300

    ajuste na posicao do icone de logout

--- src/components/SideMenuAdmin/index.jsx
+++ src/components/SideMenuAdmin/index.jsx
@@ -22,7 +22,7 @@ export function SideMenuAdmin({ path }) {
         </ItemContainer>
       ))}
       <hr></hr>
-      <ItemContainer style={{ position: 'absolute', bottom: '30px' }}>
+      <ItemContainer style={{ position: 'fixed', bottom: '2px' }}>
         <LogoutIcon style={{ color: '#ffffff', marginRight: '8px' }} />
         <Link
           to="/login"
